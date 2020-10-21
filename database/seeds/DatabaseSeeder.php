d6838c1e5f616de269b51dc863fd22cc2fff86c9
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-10-21 17:22:59 +0100

    LanguageSeeder created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MethodSeeder::class);
         $this->call(CitySeeder::class);
         $this->call(QuarterSeeder::class);
+        $this->call(LanguageSeeder::class);
     }
 }
---
+++ database/seeds/LanguageSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Language;
+use Illuminate\Database\Seeder;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $languages = [
+            [
+                'name' => 'Français',
+                'abbr' => 'fr',
+                'flag' => 'FR'
+            ],
+            [
+                'name' => 'English',
+                'abbr' => 'en',
+                'flag' => 'GB'
+            ],
+        ];
+
+        foreach ($languages as $language) {
+            Language::create($language);
+        }
+    }
+}
